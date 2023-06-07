cbd67a9c5238f0730fefa7e6cead052801319491
Oscar Toledo <oscar.canet@energia.ee>
2023-06-07 15:28:32 +0300

    Fix entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@
 echo "*************************************************************************************"
 echo "*********** Starting Application *****************"
 echo "*************************************************************************************"
-java -Djava.security.egd=file:/dev/./urandom -javaagent:/app/apm-agent.jar -Delastic.apm.log_format_sout=JSON -jar /app/app.jar
+java -Djava.security.egd=file:/dev/./urandom -jar /app/app.jar
